ed6989fdfd21fd7ed9a6c2cb82822feffe201ff0
Suryansh Pajiala <63901097+Sunny9899@users.noreply.github.com>
2021-11-08 15:50:25 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,11 @@
+function nav(){
+return `
+<a href="index.html">GoBack</a>           
+<a href="menu.html">Menu</a>
+<a href="login.html">SignIn</a>
+<a href="signup.html">SignUp</a>
+<a href="cart.html">Cart</a> 
+`
+}
+
+export default nav; 
